46e62a05f1f732981a2552e42260ec9d2a747f7b
apulecio <andres.pulecio@anthology.com>
2026-03-23 22:10:18 -0500

    Refactor code structure for improved readability and maintainability

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # basic-Infra-terraform-AWS
+Infraestructura Básica con Terraform + AWS
+
+Objetivo: Montar desde cero una VPC en AWS, con subnets públicas/privadas, Internet Gateway, NAT Gateway y EC2.
+
+Skills: Terraform (módulos, workspaces), AWS VPC, IAM, tagging, cost‑management básico.
+Entregables:
+Código Terraform reutilizable (módulo “network”).
+Documentación de variables y outputs.
+Reporte de optimización de costos (AWS Cost Explorer).
+
+![alt text](image.png)
--- deploy/main.tf
+++ deploy/main.tf
@@ -27,3 +27,29 @@ resource "aws_internet_gateway" "basic-infra" {
     Name = "basic-infra"
   }
 }
+
+resource "aws_subnet" "basic-infra" {
+  vpc_id            = aws_vpc.basic-infra.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "basic-infra"
+  }
+}
+resource "aws_route_table" "public-subnet" {
+  vpc_id = aws_vpc.basic-infra.id
+
+  tags = {
+    Name = "basic-infra"
+  }
+}
+resource "aws_route_table_association" "public-subnet" {
+  subnet_id      = aws_subnet.basic-infra.id
+  route_table_id = aws_route_table.public-subnet.id
+}   
+resource "aws_route" "public-subnet" {
+  route_table_id         = aws_route_table.public-subnet.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.basic-infra.id
+}
